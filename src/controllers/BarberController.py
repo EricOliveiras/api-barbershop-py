994dacc4d2bafcb290d099e9c283d89fe0ea9026
Eric Diego Oliveira do Nascimento <88443797+EricOliveiras@users.noreply.github.com>
2021-12-21 16:49:28 -0300

    created barberController

---
+++ src/controllers/BarberController.py
@@ -0,0 +1,37 @@
+from sqlalchemy.orm import Session
+
+from src.database.models.Barber import Barber
+from src.database.schemas.BarberBase import BarberBase
+
+class BarberController:
+  def __init__(self, db: Session):
+    self.db = db
+
+  def create_barber(self, BarberBase: BarberBase):
+
+    barber_exists = self.db.query(Barber).filter(Barber.document == BarberBase.document).first()
+
+    if not barber_exists:
+      db_barber = Barber(
+        name = BarberBase.name,
+        document = BarberBase.document,
+        phone = BarberBase.phone
+      )
+
+      self.db.add(db_barber)
+      self.db.commit()
+      self.db.refresh(db_barber)
+
+      return db_barber
+
+    else:
+      return f'Barber already exists'
+
+  def get_all_barbers(self):
+
+    all_barbers = self.db.query(Barber).all()
+
+    if all_barbers.__len__() == 0:
+      return f'No barbers found'
+    else:
+      return all_barbers
